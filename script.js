1ddddb4730ea5bd006810cb12edc8115be74c6f3
Mei <78681585+vluy5@users.noreply.github.com>
2023-11-03 05:11:04 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -9,32 +9,13 @@ playButton.addEventListener("click", () => {
     videoWindow.moveTo(0, 0);
     videoWindow.resizeTo(screen.width, screen.height);
     
-    // Добавляем обработчик для попытки закрытия окна
+    // Noooooooooooooooooooooooooooooooooooooo
     videoWindow.onbeforeunload = function(event) {
-      const confirmationMessage = "Вы уверены, что хотите закрыть окно? Возможно, вы потеряете данные.";
-      (event || window.event).returnValue = confirmationMessage; // Для совместимости с разными браузерами
+      const confirmationMessage = "GG";
+      (event || window.event).returnValue = confirmationMessage; 
       return confirmationMessage;
     };
   }
 });
 
-// Добавляем обработчик для клавиши F11
-window.addEventListener("keydown", (event) => {
-  if (event.key === "F11") {
-    event.preventDefault(); // Предотвращаем действие по умолчанию
-    const videoUrl = "rik.mp4";
-    videoWindow = window.open(videoUrl, "Video Player", "fullscreen=yes,location=no,menubar=no,status=no,toolbar=no");
-    
-    if (videoWindow) {
-      videoWindow.moveTo(0, 0);
-      videoWindow.resizeTo(screen.width, screen.height);
-      
-      // Добавляем обработчик для попытки закрытия окна
-      videoWindow.onbeforeunload = function(event) {
-        const confirmationMessage = "Вы уверены, что хотите закрыть окно? Возможно, вы потеряете данные.";
-        (event || window.event).returnValue = confirmationMessage; // Для совместимости с разными браузерами
-        return confirmationMessage;
-      };
-    }
-  }
-});
+
